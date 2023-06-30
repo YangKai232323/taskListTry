ee44d18ae144fd0b9aef2a5f9ee89f32e03b531e
YangKai232323 <rodontop1@gmail.com>
2023-06-30 19:11:21 +0300

    feat || active/complete menu + fix input issue

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ interface Task {
 }
 
 function App() {
-    const currentSiteState = 'active'
+    // const currentSiteState = 'completed'
+    const [currentSiteState, changeSiteState] = useState<string>('active')
 
     const [tasks, setTasks] = useState<Task[]>([
         {
@@ -20,7 +21,11 @@ function App() {
         },
     ])
 
-    const [inputTaskName, setInputTaskName] = useState<string>()
+    const [inputTaskName, setInputTaskName] = useState<string>('')
+
+    function clearInput(inputTaskName: string) {
+        setInputTaskName('')
+    }
 
     function addTask(task: Task) {
         setTasks([...tasks, task])
@@ -40,120 +45,160 @@ function App() {
 
     console.log(tasks)
 
-    return (
-        <>
-            <div>
-                <button className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4">
-                    Active
-                </button>
-                <button className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4">
-                    Completed
-                </button>
-            </div>
-            <div>
-                <ul>
-                    {completedTasks.map((task, taskIndex) => (
-                        <li className="rounded-xl font-bold m-4 text-2xl w-fit p-2 border-gray-500 border-4 flex items-center">
-                            {task.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="rounded-2xl border-neutral-500 bg-zinc-300 border-8 m-4 w-fit h-fit">
-                <ul>
-                    {tasks.map((task, taskIndex) => (
-                        <li className="rounded-xl font-bold m-4 text-2xl w-fit p-2 border-gray-500 border-4 flex items-center">
-                            {task.name}
-                            {task.state && (
-                                <small className="mx-1">completed</small>
-                            )}
-                            <button
-                                type="button"
-                                className="hover:rounded-md hover:scale-75 text-3xl border-black border-2 m-2"
-                                onClick={() => {
-                                    setTasks(
-                                        // tasks.filter(
-                                        //     (_, idx) => idx != taskIndex
-                                        // )
+    if (currentSiteState === 'completed') {
+        return (
+            <>
+                <div>
+                    <button
+                        className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4"
+                        onClick={() => {
+                            changeSiteState('active')
+                            clearInput(inputTaskName)
+                        }}
+                    >
+                        Active
+                    </button>
+                    <button
+                        className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4"
+                        onClick={() => {
+                            changeSiteState('completed')
+                            clearInput(inputTaskName)
+                        }}
+                    >
+                        Completed
+                    </button>
+                </div>
+                <div>
+                    <ul>
+                        {completedTasks.map((task, taskIndex) => (
+                            <li className="rounded-xl font-bold m-4 text-2xl w-fit p-2 border-gray-500 border-4 flex items-center">
+                                {task.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </>
+        )
+    } else if (currentSiteState === 'active') {
+        return (
+            <>
+                <div>
+                    <button
+                        className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4"
+                        onClick={() => {
+                            changeSiteState('active')
+                            clearInput(inputTaskName)
+                        }}
+                    >
+                        Active
+                    </button>
+                    <button
+                        className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4"
+                        onClick={() => {
+                            changeSiteState('completed')
+                            clearInput(inputTaskName)
+                        }}
+                    >
+                        Completed
+                    </button>
+                </div>
+                <div className="rounded-2xl border-neutral-500 bg-zinc-300 border-8 m-4 w-fit h-fit">
+                    <ul className="border-b-8 border-slate-400">
+                        {tasks.map((task, taskIndex) => (
+                            <li className="rounded-xl font-bold m-4 text-2xl w-fit p-2 border-gray-500 border-4 flex items-center">
+                                {task.name}
+                                {task.state && (
+                                    <small className="mx-1">completed</small>
+                                )}
+                                <button
+                                    type="button"
+                                    className="hover:rounded-md hover:scale-75 text-3xl border-black border-2 m-2"
+                                    onClick={() => {
+                                        setTasks(
+                                            // tasks.filter(
+                                            //     (_, idx) => idx != taskIndex
+                                            // )
 
-                                        tasks.map((task, idx) => {
-                                            // if (taskIndex === idx) {
-                                            //     task.state = true
-                                            // }
-                                            // return task
-                                            if (taskIndex === idx) {
-                                                task.state = true
-                                                addCompletedTask(task)
-                                            }
-                                            return task
-                                        })
-                                    )
-                                    setTasks(
-                                        tasks.filter(
-                                            (_, idx) => idx != taskIndex
+                                            tasks.map((task, idx) => {
+                                                // if (taskIndex === idx) {
+                                                //     task.state = true
+                                                // }
+                                                // return task
+                                                if (taskIndex === idx) {
+                                                    task.state = true
+                                                    addCompletedTask(task)
+                                                }
+                                                return task
+                                            })
                                         )
-                                    )
-                                }}
-                            >
-                                <CheckIcon className="w-7 h-7" />
-                            </button>
-                            <button
-                                type="button"
-                                className="hover:rounded-md hover:scale-75 text-3xl border-black border-2 m-2"
-                                onClick={() => {
-                                    setTasks(
-                                        tasks.filter(
-                                            (_, idx) => idx != taskIndex
+                                        setTasks(
+                                            tasks.filter(
+                                                (_, idx) => idx != taskIndex
+                                            )
                                         )
-                                    )
-                                }}
-                            >
-                                <XMarkIcon className="w-7 h-7" />
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-                <input
-                    type="text"
-                    placeholder="Enter task name"
-                    onInput={(event: any) => {
-                        setInputTaskName(event.target.value)
-                    }}
-                />
-                <button
-                    type="button"
-                    className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4 mr-0"
-                    onClick={() => {
-                        if (
-                            inputTaskName === '' ||
-                            inputTaskName === undefined ||
-                            inputTaskName === null
-                        ) {
-                            alert('Please, enter something before add task')
-                        } else {
-                            addTask({
-                                name: inputTaskName || '',
-                                state: false,
-                                value: 100000,
-                            })
-                        }
-                    }}
-                >
-                    Add Task
-                </button>
+                                    }}
+                                >
+                                    <CheckIcon className="w-7 h-7" />
+                                </button>
+                                <button
+                                    type="button"
+                                    className="hover:rounded-md hover:scale-75 text-3xl border-black border-2 m-2"
+                                    onClick={() => {
+                                        setTasks(
+                                            tasks.filter(
+                                                (_, idx) => idx != taskIndex
+                                            )
+                                        )
+                                    }}
+                                >
+                                    <XMarkIcon className="w-7 h-7" />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                    <input
+                        className="text-2xl p-1 m-1 ml-3 rounded-lg border-slate-500 border-4 items-center justify-center flex mt-4"
+                        type="text"
+                        placeholder="Enter task name"
+                        onInput={(event: any) => {
+                            setInputTaskName(event.target.value)
+                        }}
+                    />
+                    <button
+                        type="button"
+                        className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4 mr-0"
+                        onClick={() => {
+                            if (
+                                inputTaskName === '' ||
+                                inputTaskName === undefined ||
+                                inputTaskName === null
+                            ) {
+                                alert('Please, enter something before add task')
+                            } else {
+                                addTask({
+                                    name: inputTaskName || '',
+                                    state: false,
+                                    value: 100000,
+                                })
+                            }
+                        }}
+                    >
+                        Add Task
+                    </button>
 
-                <button
-                    type="button"
-                    className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4"
-                    onClick={() => {
-                        setTasks([])
-                    }}
-                >
-                    Delete All Tasks
-                </button>
-            </div>
-        </>
-    )
+                    <button
+                        type="button"
+                        className="font-bold m-4 text-xl bg-green-200 p-1 border-green-700 border-4"
+                        onClick={() => {
+                            setTasks([])
+                        }}
+                    >
+                        Delete All Tasks
+                    </button>
+                </div>
+            </>
+        )
+    }
     // ✔✔✓🗸✔
 }
 
