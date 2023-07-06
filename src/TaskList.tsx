a915f2e5bc40291cbbcf9761538c06a3061634a0
YangKai232323 <rodontop1@gmail.com>
2023-07-06 21:50:32 +0300

    fix || try #3 to fix mobile interface

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -57,7 +57,7 @@ export function TaskList({
                     <input
                         value={inputTaskName}
                         id="taskInput"
-                        className="text-2xl p-1 m-1 ml-3 rounded-lg border-slate-500 border-4 items-center justify-center flex mt-4"
+                        className="text-2xl p-1 m-1 ml-3 rounded-lg border-slate-500 border-4 items-center justify-center flex mt-4 w-20"
                         type="text"
                         placeholder="Enter task name"
                         onInput={(event: any) => {
